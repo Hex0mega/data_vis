dbf47447c9377494b217f6ce2e935fa90a388364
Hex0mega <vlsu8752@colorado.edu>
2017-04-27 16:46:34 -0600

    added some url formatting for data access

--- nav/projects.php
+++ nav/projects.php
@@ -5,7 +5,7 @@
   <html>
 
   <head>
-     <link href="/css/page.css" rel=stylesheet type="text/css">
+    <link href="/css/page.css" rel=stylesheet type="text/css">
     <title>Projects</title>
   </head>
 
@@ -16,27 +16,47 @@
       </p>
     </div>
   </body>
-  
- <script>
- 
- var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
+
+  <script>
+    var DateFormat = function(date) {
+      var day = date.getDate();
+      var month = date.getMonth();
+      var year = date.getFullYear();
+
+      return year + '-' + month + '-' + day;
+    }
+
+    var HttpClient = function() {
+      this.get = function(aUrl, aCallback) {
         var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
+        anHttpRequest.onreadystatechange = function() {
+          if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+            aCallback(anHttpRequest.responseText);
         }
 
-        anHttpRequest.open( "GET", aUrl, true );            
-        anHttpRequest.send( null );
+        anHttpRequest.open("GET", aUrl, true);
+        anHttpRequest.send(null);
+      }
     }
-}
 
-var client = new HttpClient();
-client.get('https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=Hwre8eap-C9y6Yuofwn9',function(response){
-  alert(response);
-});
+    var stock = 'AAPL';
+    var DAYS_BEFORE = 7;
+    var DAYS_AFTER = 3;
+    var epsDate = new Date(2010, 10, 18);
+    var startDate = new Date(epsDate);
+    startDate.setDate(startDate.getDate() - DAYS_BEFORE);
+    var endDate = new Date(epsDate);
+    endDate.setDate(endDate.getDate() + DAYS_AFTER);
+    var client = new HttpClient();
+    var url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stock + '/data.json?' + 'start_date=' + DateFormat(startDate) +
+      '&end_date=' + DateFormat(endDate) +
+      '&api_key=Hwre8eap-C9y6Yuofwn9'
 
- </script>
+
+    client.get(url,
+      function(response) {
+        alert(response);
+      });
+  </script>
 
   </html>
